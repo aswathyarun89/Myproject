32265cc50559bf166124a9f2fd0a07098a3138bd
aswathyarun89 <aswathy.sa2@gmail.com>
2020-11-05 09:39:39 +0000

    first commit

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'sl_r4wm7c=r=vc3^^yr*av8#k#a!ln-=pu(14j4jx4!9@tjzo7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
